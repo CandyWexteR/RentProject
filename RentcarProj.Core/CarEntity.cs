669c860d65b0c4ff92b1207c4a7ece7575c173ec
Лев <alexgammerpro@mail.ru>
2022-02-05 22:10:19 +0700

    Были добавлены свойства класса: год выпуска, гос номер. Были добавлены комментарии.

--- RentcarProj.Core/CarEntity.cs
+++ RentcarProj.Core/CarEntity.cs
@@ -6,13 +6,38 @@ using System.Threading.Tasks;
 
 namespace RentcarProj.Core
 {//TODO:Доработать сущность машины
+    
+    /// <summary>
+    /// Описание сущности автомобиля.
+    /// </summary>
     public class CarEntity
     {
+        /// <summary>
+        /// Идентификатор записи.
+        /// </summary>
         public Guid Id { get; set; }
+        /// <summary>
+        /// Марка автомобиля.
+        /// </summary>
         public string Mark { get; set; }
+        /// <summary>
+        /// Цвет автомобиля.
+        /// </summary>
         public string Color { get; set; }
+        /// <summary>
+        /// Тип коробки передач.
+        /// </summary>
         public bool IsMechanic { get; set;}
+        /// <summary>
+        /// Год выпуска автомобиля.
+        /// </summary>
+        public DateTime ReleaseYear { get; set; }
+        /// <summary>
+        /// Номер машины. Должен состоять из 9 символов, первые 6 из которых являются номером автомобиля, а последние 3 - номером региона.
+        /// </summary>
+        public char[] StateNumber { get; set; }
         
+
 
 
     }
