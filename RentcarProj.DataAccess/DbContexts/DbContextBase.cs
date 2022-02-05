ee3e39e3f44f5a46708b2ee6e6705dc3e432d2ef
shoot <mr.fostik@mail.ru>
2022-02-05 20:50:11 +0700

    Создан контекст базы данных

---
+++ RentcarProj.DataAccess/DbContexts/DbContextBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RentcarProj.DataAccess.DbContexts;
+
+public class DbContextBase<T>:DbContext where T:class
+{
+    public DbContextBase(DbContextOptions options):base(options)
+    {
+        Database.EnsureCreated();
+    }
+    
+    public DbSet<T> Table { get; }
+}
