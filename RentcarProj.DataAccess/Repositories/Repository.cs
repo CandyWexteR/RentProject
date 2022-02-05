7d53ba8c60b7fb20854590d8a158b3f5b71844a8
shoot <mr.fostik@mail.ru>
2022-02-05 21:05:33 +0700

    Добавлены комментарии

--- RentcarProj.DataAccess/Repositories/Repository.cs
+++ RentcarProj.DataAccess/Repositories/Repository.cs
@@ -7,22 +7,25 @@ namespace RentcarProj.DataAccess.Repositories;
 
 public class Repository<T>:IRepository<T> where T:class
 {
-    private DbContextBase<T?> _context;
-    public Repository(DbContextBase<T?> context)
+    private DbContextBase<T> _context;
+    public Repository(DbContextBase<T> context)
     {
         _context = context;
     }
-
+    
+    /// <inheritdoc/>
     public async Task<T?> Get(Guid id, CancellationToken cancellationToken)
     {
         return await _context.Table.FindAsync(id);
     }
 
+    /// <inheritdoc/>
     public async Task Add(T entity, CancellationToken cancellationToken)
     {
         await _context.AddAsync(entity, cancellationToken);
     }
 
+    /// <inheritdoc/>
     public async Task Remove(Guid id, CancellationToken cancellationToken)
     {
         var item = await _context.Table.FindAsync(id, cancellationToken);
@@ -30,14 +33,16 @@ public class Repository<T>:IRepository<T> where T:class
             _context.Remove(item);
     }
 
+    /// <inheritdoc/>
     public async Task Update(T entity, CancellationToken cancellationToken)
     {
         if (!cancellationToken.IsCancellationRequested)
             _context.Table.Update(entity);
     }
 
-    public async Task<IQueryable<T>> GetAllAsNoTracking(Expression<Func<T?, bool>> predicate)
+    /// <inheritdoc/>
+    public async Task<IQueryable<T>> GetAllAsNoTracking(Expression<Func<T?, bool>>? predicate)
     {
-        return _context.Table.Where(predicate).AsNoTracking();
+        return predicate is not null ? _context.Table.Where(predicate).AsNoTracking() : _context.Table.AsNoTracking();
     }
 }
