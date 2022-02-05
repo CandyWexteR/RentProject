c5b890c2427a87321ca3eed4e63b3b13db65d096
shoot <mr.fostik@mail.ru>
2022-02-05 20:50:31 +0700

    Создан шаблонный репозиторий

---
+++ RentcarProj.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace RentcarProj.DataAccess.Repositories;
+
+public interface IRepository<T>
+{
+    public Task<T?> Get(Guid id, CancellationToken cancellationToken);
+    public Task Add(T entity, CancellationToken cancellationToken);
+    public Task Remove(Guid id, CancellationToken cancellationToken);
+    public Task Update(T entity, CancellationToken cancellationToken);
+    public Task<IQueryable<T>> GetAllAsNoTracking(Expression<Func<T?, bool>> predicate);
+}
---
+++ RentcarProj.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using RentcarProj.DataAccess.DbContexts;
+
+namespace RentcarProj.DataAccess.Repositories;
+
+public class Repository<T>:IRepository<T> where T:class
+{
+    private DbContextBase<T?> _context;
+    public Repository(DbContextBase<T?> context)
+    {
+        _context = context;
+    }
+
+    public async Task<T?> Get(Guid id, CancellationToken cancellationToken)
+    {
+        return await _context.Table.FindAsync(id);
+    }
+
+    public async Task Add(T entity, CancellationToken cancellationToken)
+    {
+        await _context.AddAsync(entity, cancellationToken);
+    }
+
+    public async Task Remove(Guid id, CancellationToken cancellationToken)
+    {
+        var item = await _context.Table.FindAsync(id, cancellationToken);
+        if (item is not null && !cancellationToken.IsCancellationRequested)
+            _context.Remove(item);
+    }
+
+    public async Task Update(T entity, CancellationToken cancellationToken)
+    {
+        if (!cancellationToken.IsCancellationRequested)
+            _context.Table.Update(entity);
+    }
+
+    public async Task<IQueryable<T>> GetAllAsNoTracking(Expression<Func<T?, bool>> predicate)
+    {
+        return _context.Table.Where(predicate);
+    }
+}
