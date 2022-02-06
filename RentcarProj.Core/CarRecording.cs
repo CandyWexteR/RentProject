39d0a8734ac4b1d2b6d7a45bf72dbefe669f0a2b
Лев <alexgammerpro@mail.ru>
2022-02-06 11:52:19 +0700

    Создана  сущность описания записи автомобиля. Добавлены комментарии.

---
+++ RentcarProj.Core/CarRecording.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentcarProj.Core
+//TODO:Выполнить работу над ошибками и осознать выполненную работу.Дописать эту сущность.
+{
+    /// <summary>
+    /// Описание сущности записи автомобиля.
+    /// </summary>
+     class CarRecording
+    {
+        /// <summary>
+        /// Запись в базе данных.
+        /// </summary>
+       public Guid Id { get; set; }
+
+        /// <summary>
+        /// Запись автомобиля в базе данных.
+        /// </summary>
+        public Guid CarId { get; set; }
+        /// <summary>
+        /// Время окончания аренды автомобиля.
+        /// </summary>
+        
+        public DateTime? EndOfRent  { get; set; }
+       
+        /// <summary>
+        /// Стоиомость аренды автомобиля за месяц.
+        /// </summary>
+        public uint CostPerMonth { get; set; }
+
+
+
+
+
+    }
+}
