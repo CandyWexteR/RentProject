226457bc2b27a4638433e07e89aa806fde478716
Лев <alexgammerpro@mail.ru>
2022-02-13 13:07:43 +0700

    Были созданы  базовое  событие и событие аренды . Были созданы свойства для них.

---
+++ RentcarProj.Core/Events/CarIsRented.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentcarProj.Core
+{
+    public class CarIsRented:EventBase
+    {
+        protected CarIsRented(Guid id, DateTime timeStamp) : base(id, timeStamp)
+        {
+        }
+
+        public string UserData { get; set; }
+        public Guid CarId  { get; set; }
+
+    }
+}
---
+++ RentcarProj.Core/Events/EventBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentcarProj.Core
+{
+    public class EventBase
+    
+    {
+        protected EventBase(Guid id, DateTime timeStamp)
+        {
+            Id = id;
+            TimeStamp = timeStamp;
+
+        }
+        public Guid Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+
+}
