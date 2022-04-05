3e79681d8afa12d6dee229d9f709672365581291
shoot <malyshev.a.588-1@e.tusur.ru>
2022-04-05 19:49:26 +0700

    Добавлены комментарии к базовому обработчику запросов

--- RentcarProj.BuildingBlocks/QueryBus/IQueryHandlerBase.cs
+++ RentcarProj.BuildingBlocks/QueryBus/IQueryHandlerBase.cs
@@ -2,6 +2,11 @@
 
 namespace RentcarProj.BuildingBlocks.QueryBus;
 
+/// <summary>
+/// Базовый обработчик запроса
+/// </summary>
+/// <typeparam name="TRequest">Тип запроса</typeparam>
+/// <typeparam name="TResponse">Тип ожидаемого ответа</typeparam>
 public interface IQueryHandlerBase<TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
     where TRequest : IQueryBase<TResponse>
     where TResponse : class
