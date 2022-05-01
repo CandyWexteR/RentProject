32c9da3d44527ae14618ce071534addd6490d374
shoot <mr.fostik@mail.ru>
2022-05-01 11:01:53 +0700

    Дополнены сущности

--- OnlineShop/OnlineShop.Core/Entities/Info.cs
+++ OnlineShop/OnlineShop.Core/Entities/Info.cs
@@ -1,6 +1,64 @@
-﻿namespace OnlineShop.Core.Entities;
+﻿using OnlineShop.Core.Configurations;
+
+namespace OnlineShop.Core.Entities;
 
 public class Info
 {
-    public List<Field> Type { get; set; }
+    private List<Field> _fields;
+
+    protected Info(List<Field> fields)
+    {
+        _fields = fields;
+    }
+
+    public List<Field> Fields
+    {
+        get => _fields;
+        protected set
+        {
+            _fields = new List<Field>();
+            _fields.AddRange(value);
+        }
+    }
+
+    public static Info Create(InfoConfiguration configuration, Dictionary<string, string> fieldValues)
+    {
+        //TODO: Exception на свой тип
+        var exceptions = new List<Exception>();
+
+        var selectedFieldNames = fieldValues.Keys.Select(f => f);
+
+        //Выборка обязательных полей, не вошедших в словарь
+        var requiredFieldsWithoutValue =
+            configuration.Fields
+                .Where(f => f.IsRequired && !selectedFieldNames.Contains(f.Name) && f.DefaultValue == null).ToArray();
+        if (requiredFieldsWithoutValue.Any())
+        {
+            foreach (var item in requiredFieldsWithoutValue)
+            {
+                exceptions.Add(new Exception("В переданном списке не содержится"));
+            }
+        }
+
+        if (exceptions.Any())
+            throw new AggregateException(exceptions);
+
+        var fieldsList = new List<Field>();
+
+        foreach (var fieldConfiguration in configuration.Fields)
+        {
+            var field = new Field()
+            {
+                Name = fieldConfiguration.Name,
+                Value = fieldConfiguration.DefaultValue ?? throw new Exception("Попытка установки NULL-значения")
+            };
+
+            if (fieldValues.ContainsKey(fieldConfiguration.Name))
+                field.Value = fieldValues[fieldConfiguration.Name];
+            
+            fieldsList.Add(field);
+        }
+
+        return new Info(fieldsList);
+    }
 }
---
+++ OnlineShop/OnlineShop.Core/Entities/Item.cs
@@ -0,0 +1,31 @@
+﻿using OnlineShop.Core.Configurations;
+
+namespace OnlineShop.Core.Entities;
+
+//TODO: Инкапсуляция
+public class Item
+{
+    protected Item(Guid id, ItemCategories category, Info info)
+    {
+        Id = id;
+        Category = category;
+        Info = info;
+    }
+
+    public Guid Id { get; set; }
+    public ItemCategories Category { get; set; }
+    public Info Info { get; set; }
+
+    public static Item Create(Guid id, ItemCategories category, InfoConfiguration infoConfiguration, Dictionary<string,string> fieldValues)
+    {
+        var info = Info.Create(infoConfiguration, fieldValues);
+
+        if (!Enum.IsDefined(typeof(ItemCategories), category))
+            throw new Exception("Несуществующая категория");
+
+        if (id == Guid.Empty)
+            throw new Exception("Идентификатор не может быть пустым");
+        
+        return new Item(id, category, info);
+    }
+}
